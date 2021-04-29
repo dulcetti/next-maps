443afaf7dc642f2e8b43cefc30bde999543c4d8e
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-04-28 23:10:30 -0300

    feat: add link wrapper on main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,3 +1,7 @@
+import { Beer } from '@styled-icons/ionicons-outline/Beer';
+// import { CloseCircle } from '@styled-icons/ionicons-outline/CloseCircle';
+
+import LinkWrapper from 'components/link-wrapper';
 import dynamic from 'next/dynamic';
 import * as S from './styles';
 
@@ -6,6 +10,9 @@ const Map = dynamic(() => import('components/map'), { ssr: false });
 export default function Main() {
   return (
     <S.Wrapper>
+      <LinkWrapper href="/about">
+        <Beer size={32} />
+      </LinkWrapper>
       <Map />
     </S.Wrapper>
   );
