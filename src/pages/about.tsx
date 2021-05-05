8a7f45d8e8ff5e6b8fd64ddb2e2dca678c0d8cdd
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-05-04 23:28:22 -0300

    feat: add getStaticPaths to get all pages

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,16 @@
-import AboutTemplate from '../templates/about/index';
+import PageTemplate from '../templates/page/index';
+import client from '../graphql/client';
+import { GET_PAGES } from '../graphql/queries';
 
 export default function About() {
-  return <AboutTemplate />;
+  return <PageTemplate />;
+}
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 });
+  const paths = pages.map(({ slug }) => ({
+    params: { slug },
+  }));
+
+  return { paths, fallback: true };
 }
