3d8eb727ea7054a482ad9c77b0085f5156166310
Bruno Dulcetti <brunodulcetti@gmail.com>
2021-06-11 20:16:57 -0300

    feat: make the location dinamicaly - import hooks - create lat and long states - add useEffect to update infos

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,16 +1,29 @@
+import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 import { MapProps } from './interface';
+import MyMap from './map';
 
 export default function Map({ places }: MapProps) {
+  const [latitude, changeLatitude] = useState(-18);
+  const [longitude, changeLongitude] = useState(-50);
+
+  useEffect(() => {
+    const latitude = places[0].location?.latitude || 0;
+    const longitude = places[0].location?.longitude || 0;
+
+    changeLatitude(latitude);
+    changeLongitude(longitude);
+  }, [places]);
+
   return (
-    <MapContainer center={[0, 0]} zoom={3} style={{ height: '100%', width: '100%' }}>
+    <MapContainer center={[latitude, longitude]} zoom={4} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {places?.map(({ id, name, location }) => {
-        const { latitude, longitude } = location;
+      <MyMap latitude={latitude} longitude={longitude} />
+      {places?.map(({ id, name }) => {
         return <Marker position={[latitude, longitude]} title={name} key={`place-${id}`} />;
       })}
     </MapContainer>
